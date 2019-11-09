8aec31eea6da5aaede6c392cb14e4a6119f074e4
tetsu <inaba.t@a.phys.nagoya-u.ac.jp>
2019-11-09 11:23:16 +0900

    methods-and-interfaces

---
+++ Methods-and-interfaces/methods-pointers.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (v *Vertex) Scale(f float64) { //ポインタにするとline 23での代入が反映される。
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func main() {
+	v := Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v.Abs()) // *Vertex -> 50, Vertex -> 5
+}
